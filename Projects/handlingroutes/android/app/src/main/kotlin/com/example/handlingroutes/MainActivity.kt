45ec01dfb08b670777778b15b7922e7d894e272e
Bilal Ahmad <74393555+bilalahmad9@users.noreply.github.com>
2020-11-19 18:59:47 +0500

    My commit

---
+++ Projects/handlingroutes/android/app/src/main/kotlin/com/example/handlingroutes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.handlingroutes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/different_project_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.different_project_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
